4f2725472f13017d4115651e210f6de15492f47f
DuZF <du@mail.bnu.edu.cn>
2015-10-29 15:23:45 +0800

    Correct the inner version lavel

--- MainEngine.c
+++ MainEngine.c
@@ -379,7 +379,7 @@ int main(int argc, char *argv[])
 
   int K = 0;
   char scheme[100];
-  char version[100] = "LibSepatating";
+  char version[100] = "dev : a stable version";
   printf("The present version is [%s].\n", version);
   //*
   K = GRP_HWENO_fix(CONFIG, OPT, m, h, rho, u, p, &runhist, scheme);
