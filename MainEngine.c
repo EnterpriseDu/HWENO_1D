f25de9f0cca5103f880826619ed4035e50c82aeb
DuZF@HP <du@mail.bnu.edu.cn>
2016-06-11 16:49:51 +0800

    Add some comments

--- MainEngine.c
+++ MainEngine.c
@@ -347,6 +347,8 @@ int main(int argc, char *argv[])
   //K = LF4_WENO_fix(CONFIG, OPT, m, h, rho, u, p, &runhist, scheme);
   //K = GRP4_HWENO_fix(CONFIG, OPT, m, h, rho, u, p, &runhist, scheme);
   K = THINC_primitive_fix(CONFIG, OPT, m, h, rho, u, p, &runhist, scheme);
+  //K = THINC_conservative_fix(CONFIG, OPT, m, h, rho, u, p, &runhist, scheme);
+  //K = GRP2_fix(CONFIG, OPT, m, h, rho, u, p, &runhist, scheme);
 
 
   int vvM = vM;
