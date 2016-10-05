f1ab7d4553edd689f81d382a64a0c9ab65fa0605
duzf@surface <du@mail.bnu.edu.cn>
2016-10-05 23:15:26 +0800

    Add a new GRP5-WENO5 solver
    The interpolation of the cell boundary value along the time direction
    remains to be decided.

--- GRP_WENO_fix/GRP5_WENO5_fix.c
+++ GRP_WENO_fix/GRP5_WENO5_fix.c
@@ -12,13 +12,13 @@
 #include "reconstruction.h"
 
 
-int GRP4_WENO5_fix
+int GRP5_WENO5_fix
 (double const CONFIG[], double const OPT[], int const m, double const h,
  double *rho[], double *u[], double *p[], runList *runhist, char *scheme)
 {
   int i = 0, j = 0, k = 1, it = 0;
 
-  char scheme_local[50] = "G4W5\0";
+  char scheme_local[50] = "G5W5\0";
   printf("===========================\n");
   printf("The scheme [%s] started.\n", scheme_local);
   int len = 0;
@@ -69,25 +69,31 @@ int GRP4_WENO5_fix
 
   double mom[m], ene[m];
   double rho_star, u_star, p_star, E_star;
-  double rhoI[m+1], momI[m+1], eneI[m+1], uI[m+1], pI[m+1], half_rhoI[m+1], half_momI[m+1], half_eneI[m+1], half_uI[m+1], half_pI[m+1];
+  double rhoI[m+1], momI[m+1], eneI[m+1], uI[m+1], pI[m+1];//, half_rhoI[m+1], half_momI[m+1], half_eneI[m+1], half_uI[m+1], half_pI[m+1];
+  double rhoI1[m+1], momI1[m+1], eneI1[m+1], uI1[m+1], pI1[m+1];
+  double rhoI2[m+1], momI2[m+1], eneI2[m+1], uI2[m+1], pI2[m+1];
   double rho_L[m+1], rho_R[m+1], u_L[m+1], u_R[m+1], p_L[m+1], p_R[m+1];
   double D_rho_L[m+1], D_rho_R[m+1], D_u_L[m+1], D_u_R[m+1], D_p_L[m+1], D_p_R[m+1];
 
-  double half_rho[m], half_u[m], half_p[m], half_mom[m], half_ene[m];
+  double rho1[m], u1[m], p1[m], mom1[m], ene1[m];
+  double rho2[m], u2[m], p2[m], mom2[m], ene2[m];
 
   double f01[m+1], f02[m+1], f03[m+1];
   double f11[m+1], f12[m+1], f13[m+1];
+  double f21[m+1], f22[m+1], f23[m+1];
   double g01[m+1], g02[m+1], g03[m+1];
   double g11[m+1], g12[m+1], g13[m+1];
+  double g21[m+1], g22[m+1], g23[m+1];
   double F1[m+1], F2[m+1], F3[m+1];
 
   double sigma, speed_max;  /* speed_max denote the largest character
 			     * speed at each time step
 			     */
-  double tau, half_tau, nu, half_nu, alp, bet;
+  double tau, tau1, tau2, nu, nu1, nu2, alp, bet;
 
 
-  double D0 = 1.0/6.0, D1 = 0.5-D0;
+  double D10 = -0.5, D11 = 1.5;
+  double D20 = 0.25, D21 = 25.0/36.0,  D22 = 1.0/18.0;
 
 
   if(Riemann)
@@ -109,23 +115,6 @@ int GRP4_WENO5_fix
   running_info[1] = 0.0;  // time
   running_info[2] = 0.0;  // not half
   WENO_50(running_info, m, h, eps, alp2, gamma, rho[0], mom, ene, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R);
-  /*
-  write_column(m, rho[0], "rho", "running");
-  write_column(m, ene, "ene", "running");
-  write_column(m, p[0], "p", "running");
-
-  write_column(m+1, rho_L, "rhoL", "running");
-  write_column(m+1, u_L, "uL", "running");
-  write_column(m+1, p_L, "pL", "running");
-  write_column(m+1, rho_R, "rhoR", "running");
-  write_column(m+1, u_R, "uR", "running");
-  write_column(m+1, p_R, "pR", "running");
-  write_column(m+1, D_rho_L, "DrhoL", "running");
-  write_column(m+1, D_u_L, "DuL", "running");
-  write_column(m+1, D_p_L, "DpL", "running");
-  write_column(m+1, D_rho_R, "DrhoR", "running");
-  write_column(m+1, D_u_R, "DuR", "running");
-  write_column(m+1, D_p_R, "DpR", "running");//*/
 //------------THE MAIN LOOP-------------
   for(k = 1; k <= MaxStp; ++k)
   {    
@@ -162,9 +151,9 @@ int GRP4_WENO5_fix
     }
     tau = (CFL * h) / speed_max;
     if(T+tau > TIME){tau = TIME-T; T = TIME;} else{T += tau;}
-    half_tau = 0.5*tau;
+    tau1 = 0.4*tau;
     nu = tau/h;
-    half_nu = 0.5*nu;
+    nu1 = 0.4*nu;
     runhist->current->time[0] = tau;
     //printf("%g, %g\n", tau, T);
 
@@ -193,30 +182,31 @@ int GRP4_WENO5_fix
       rhoI[j] = U[0];
       momI[j] = U[0]*U[1];
       eneI[j] = 0.5*U[0]*U[1]*U[1] + U[3]/(gamma-1.0);
-      half_rhoI[j] = rhoI[j] + half_tau*D[0];
-      half_momI[j] = momI[j] + half_tau*(D[1]*U[0] + D[0]*U[1]);
-      half_eneI[j] = eneI[j] + half_tau*(0.5*D[0]*U[1]*U[1] + U[0]*U[1]*D[1] + D[3]/(gamma-1.0));
+      //rhoI1[j] = rhoI[j] + tau1*D[0];
+      //momI1[j] = momI[j] + tau1*(D[1]*U[0] + D[0]*U[1]);
+      //eneI1[j] = eneI[j] + tau1*(0.5*D[0]*U[1]*U[1] + U[0]*U[1]*D[1] + D[3]/(gamma-1.0));
 
       uI[j] = U[1];
       pI[j] = U[3];
-      half_uI[j] = U[1] + half_tau*D[1];
-      half_pI[j] = U[3] + half_tau*D[3];
+      //uI1[j] = U[1] + tau1*D[1];
+      //pI1[j] = U[3] + tau1*D[3];
     }
     for(j = 0; j < m; ++j)
     {
-      half_rho[j] = rho[vk0][j] - half_nu*((f01[j+1]-f01[j]) + 0.5*half_tau*(g01[j+1]-g01[j]));
-      half_mom[j] =      mom[j] - half_nu*((f02[j+1]-f02[j]) + 0.5*half_tau*(g02[j+1]-g02[j]));
-      half_ene[j] =      ene[j] - half_nu*((f03[j+1]-f03[j]) + 0.5*half_tau*(g03[j+1]-g03[j]));
+      rho1[j] = rho[vk0][j] - nu1*((f01[j+1]-f01[j]) + 0.5*tau1*(g01[j+1]-g01[j]));
+      mom1[j] =      mom[j] - nu1*((f02[j+1]-f02[j]) + 0.5*tau1*(g02[j+1]-g02[j]));
+      ene1[j] =      ene[j] - nu1*((f03[j+1]-f03[j]) + 0.5*tau1*(g03[j+1]-g03[j]));
 
-      half_u[j] = half_mom[j] / half_rho[j];
-      half_p[j] = (half_ene[j] - 0.5*half_mom[j]*half_u[j]) * (gamma-1.0);
-      if(half_p[j] < 0.0)
-        printf("half(%d,%d), %g\n", k, j, half_p[j]);
+      u1[j] = mom1[j] / rho1[j];
+      p1[j] = (ene1[j] - 0.5*mom1[j]*u1[j]) * (gamma-1.0);
+      if(p1[j] < 0.0)
+        printf("half(%d,%d), %g\n", k, j, p1[j]);
     }
 
-    running_info[1] = T - half_tau;  // time
+    running_info[1] = T - tau + tau1;  // time
     running_info[2] = 1.0;           // half
-    WENO_5(running_info, m, h, eps, alp2, gamma, half_rho, half_mom, half_ene, half_rhoI, half_uI, half_pI, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R, runhist->current->trouble1);
+    //WENO_5(running_info, m, h, eps, alp2, gamma, rho1, mom1, ene1, rhoI1, uI1, pI1, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R, runhist->current->trouble1);
+    WENO_50(running_info, m, h, eps, alp2, gamma, rho1, mom1, ene1, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R);
 
 
     for(j = 0; j < m+1; ++j)
@@ -233,34 +223,75 @@ int GRP4_WENO5_fix
       g13[j] = (D[3]*U[1] + U[3]*D[1])*gamma/(gamma-1.0);
       g13[j] = g13[j] + 0.5*D[0]*U[1]*U[1]*U[1] + 1.5*U[0]*U[1]*U[1]*D[1];
 
-      F1[j] = f01[j] + tau*(D0*g01[j] + D1*g11[j]);
-      F2[j] = f02[j] + tau*(D0*g02[j] + D1*g12[j]);
-      F3[j] = f03[j] + tau*(D0*g03[j] + D1*g13[j]);
+      //rhoI2[j] = rhoI[j] + tau*D[0];
+      //momI2[j] = momI[j] + tau*(D[1]*U[0] + D[0]*U[1]);
+      //eneI2[j] = eneI[j] + tau*(0.5*D[0]*U[1]*U[1] + U[0]*U[1]*D[1] + D[3]/(gamma-1.0));
 
-      rhoI[j] = rhoI[j] + tau*D[0];
-      momI[j] = momI[j] + tau*(D[1]*U[0] + D[0]*U[1]);
-      eneI[j] = eneI[j] + tau*(0.5*D[0]*U[1]*U[1] + U[0]*U[1]*D[1] + D[3]/(gamma-1.0));
-
-      uI[j] = uI[j] + tau*D[1];
-      pI[j] = pI[j] + tau*D[3];
+      //uI2[j] = uI[j] + tau*D[1];
+      //pI2[j] = pI[j] + tau*D[3];
     }
-
-//===============THE CORE ITERATION=================
     for(j = 0; j < m; ++j)
     {
-      rho[vk1][j] = rho[vk0][j] - nu*(F1[j+1]-F1[j]);
-           mom[j] =      mom[j] - nu*(F2[j+1]-F2[j]);
-	   ene[j] =      ene[j] - nu*(F3[j+1]-F3[j]);
-	u[vk1][j] = mom[j] / rho[vk1][j];
-	p[vk1][j] = (ene[j] - 0.5*mom[j]*u[vk1][j])*(gamma-1.0);
+      rho2[j] = rho[vk0][j] - nu*((f01[j+1]-f01[j]) + 0.5*tau*(D10*(g01[j+1]-g01[j])+D11*(g11[j+1]-g11[j])));
+      mom2[j] =      mom[j] - nu*((f02[j+1]-f02[j]) + 0.5*tau*(D10*(g02[j+1]-g02[j])+D11*(g12[j+1]-g12[j])));
+      ene2[j] =      ene[j] - nu*((f03[j+1]-f03[j]) + 0.5*tau*(D10*(g03[j+1]-g03[j])+D11*(g13[j+1]-g13[j])));
+
+      u2[j] = mom2[j] / rho2[j];
+      p2[j] = (ene2[j] - 0.5*mom2[j]*u2[j])*(gamma-1.0);
 
       if(p[vk1][j] < 0.0)
         printf("    (%d,%d), %g\n", k, j, p[vk1][j]);
     }
 
     running_info[1] = T;
+    running_info[2] = 2.0;  // not half
+    //WENO_5(running_info, m, h, eps, alp2, gamma, rho2, mom2, ene2, rhoI2, uI2, pI2, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R, runhist->current->trouble0);
+    WENO_50(running_info, m, h, eps, alp2, gamma, rho2, mom2, ene2, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R);
+
+
+    for(j = 0; j < m+1; ++j)
+    {
+      linear_GRP_solver(wave_speed, D, U, 0.0, gamma, eps,
+			rho_L[j], u_L[j], 0.0, p_L[j],
+			rho_R[j], u_R[j], 0.0, p_R[j],
+			D_rho_L[j], D_u_L[j], 0.0, D_p_L[j],
+			D_rho_R[j], D_u_R[j], 0.0, D_p_R[j],
+			0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+
+      g21[j] = U[0]*D[1] + U[1]*D[0];
+      g22[j] = D[0]*U[1]*U[1] + 2.0*U[0]*U[1]*D[1] + D[3];
+      g23[j] = (D[3]*U[1] + U[3]*D[1])*gamma/(gamma-1.0);
+      g23[j] = g13[j] + 0.5*D[0]*U[1]*U[1]*U[1] + 1.5*U[0]*U[1]*U[1]*D[1];
+
+      F1[j] = f01[j] + 0.5*tau*(D20*g01[j] + D21*g11[j] + D22*g21[j]);
+      F2[j] = f02[j] + 0.5*tau*(D20*g02[j] + D21*g12[j] + D22*g22[j]);
+      F3[j] = f03[j] + 0.5*tau*(D20*g03[j] + D21*g13[j] + D22*g23[j]);
+
+      //rhoI[j] = rhoI[j] + tau*D[0];
+      //momI[j] = momI[j] + tau*(D[1]*U[0] + D[0]*U[1]);
+      //eneI[j] = eneI[j] + tau*(0.5*D[0]*U[1]*U[1] + U[0]*U[1]*D[1] + D[3]/(gamma-1.0));
+
+      //uI[j] = uI[j] + tau*D[1];
+      //pI[j] = pI[j] + tau*D[3];
+    }
+    for(j = 0; j < m; ++j)
+    {
+      rho[vk1][j] = rho[vk0][j] - nu*(F1[j+1]-F1[j]);
+           mom[j] =      mom[j] - nu*(F2[j+1]-F2[j]);
+           ene[j] =      ene[j] - nu*(F3[j+1]-F3[j]);
+
+      u[vk1][j] = mom[j] / rho[vk1][j];
+      p[vk1][j] = (ene[j] - 0.5*mom[j]*u[vk1][j])*(gamma-1.0);
+
+      if(p[vk1][j] < 0.0)
+        printf("    (%d,%d), %g\n", k, j, p[vk1][j]);
+    }
+
+
+    running_info[1] = T;
     running_info[2] = 0.0;  // not half
-    WENO_5(running_info, m, h, eps, alp2, gamma, rho[vk1], mom, ene, rhoI, uI, pI, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R, runhist->current->trouble0);
+    //WENO_5(running_info, m, h, eps, alp2, gamma, rho[vk1], mom, ene, rhoI, uI, pI, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R, runhist->current->trouble0);
+    WENO_50(running_info, m, h, eps, alp2, gamma, rho[vk1], mom, ene, rho_L, rho_R, u_L, u_R, p_L, p_R, D_rho_L, D_rho_R, D_u_L, D_u_R, D_p_L, D_p_R);
 
     toc = clock();
     runhist->current->time[1] = ((double)toc - (double)tic) / (double)CLOCKS_PER_SEC;
