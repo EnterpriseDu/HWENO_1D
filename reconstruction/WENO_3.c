2b9af5e6d93b4728378f3024fc5e1afc7638b6fc
DuZF@HP <du@mail.bnu.edu.cn>
2016-10-07 21:20:11 +0800

    Correct typos in the 3rd-order WENO reconstrution

--- reconstruction/WENO_3.c
+++ reconstruction/WENO_3.c
@@ -20,7 +20,7 @@ void WENO_30
   int const    limiter   = (int)running_info[6];
   double const threshold =      running_info[7];
   int j, k;
-  int l = 4, half_l = 2;
+  int l = 4, hl = 2;
 
   double W1[m+l], W2[m+l], W3[m+l]; //W1=rho, W2=mom, W3=ene
   double Q1[l+2], Q2[l+2], Q3[l+2];
@@ -32,9 +32,9 @@ void WENO_30
   double H_star, u_star, c_square, c_star, gamma1 = gamma-1.0;
   for(j = 0; j < m; ++j)
   {
-    W1[j+half_l] = rho[j];
-    W2[j+half_l] = mom[j];
-    W3[j+half_l] = ene[j];
+    W1[j+hl] = rho[j];
+    W2[j+hl] = mom[j];
+    W3[j+hl] = ene[j];
   }
 
   if(bod < 0)
@@ -42,27 +42,27 @@ void WENO_30
     W1[0]   = rho[1];        W1[1]   = rho[0];
     W2[0]   =-mom[1];        W2[1]   =-mom[0];
     W3[0]   = ene[1];        W3[1]   = ene[0];
-    W1[m+half_l] = rho[m-1]; W1[m+half_l+1] = rho[m-2];
-    W2[m+half_l] =-mom[m-1]; W2[m+half_l+1] =-mom[m-2];
-    W3[m+half_l] = ene[m-1]; W3[m+half_l+1] = ene[m-2];
+    W1[m+hl] = rho[m-1]; W1[m+hl+1] = rho[m-2];
+    W2[m+hl] =-mom[m-1]; W2[m+hl+1] =-mom[m-2];
+    W3[m+hl] = ene[m-1]; W3[m+hl+1] = ene[m-2];
   }
   else if(bod > 0)
   {
     W1[0]   = rho[0];        W1[1]   = rho[0];
     W2[0]   = mom[0];        W2[1]   = mom[0];
     W3[0]   = ene[0];        W3[1]   = ene[0];
-    W1[m+half_l] = rho[m-1]; W1[m+half_l+1] = rho[m-1];
-    W2[m+half_l] = mom[m-1]; W2[m+half_l+1] = mom[m-1];
-    W3[m+half_l] = ene[m-1]; W3[m+half_l+1] = ene[m-1];
+    W1[m+hl] = rho[m-1]; W1[m+hl+1] = rho[m-1];
+    W2[m+hl] = mom[m-1]; W2[m+hl+1] = mom[m-1];
+    W3[m+hl] = ene[m-1]; W3[m+hl+1] = ene[m-1];
   }
   else
   {
     W1[0]   = rho[m-2];     W1[1]   = rho[m-1];
     W2[0]   = mom[m-2];     W2[1]   = mom[m-1];
     W3[0]   = ene[m-2];     W3[1]   = ene[m-1];
-    W1[m+half_l] = rho[0];  W1[m+half_l+1] = rho[1];
-    W2[m+half_l] = mom[0];  W2[m+half_l+1] = mom[1];
-    W3[m+half_l] = ene[0];  W3[m+half_l+1] = ene[1];
+    W1[m+hl] = rho[0];  W1[m+hl+1] = rho[1];
+    W2[m+hl] = mom[0];  W2[m+hl+1] = mom[1];
+    W3[m+hl] = ene[0];  W3[m+hl+1] = ene[1];
   }
 
   for(j = 0; j < m+l; ++j)
@@ -72,7 +72,7 @@ void WENO_30
     H[j] = 0.5*u[j]*u[j] + gamma*p[j]/W1[j]/(gamma-1.0);
   }
 
-  for(j = half_l; j < m+l; ++j)
+  for(j = hl; j < m+l; ++j)
   {
     u_star = (W2[j]/sqrt(W1[j]) + W2[j-1]/sqrt(W1[j-1]))/(sqrt(W1[j]) + sqrt(W1[j-1]));
     H_star = (sqrt(W1[j])* H[j] + sqrt(W1[j-1])* H[j-1])/(sqrt(W1[j]) + sqrt(W1[j-1]));
@@ -80,7 +80,7 @@ void WENO_30
     c_star = sqrt(c_square);
 
   //=========Charactoristic Decomposition=========
-    decomposition(H_star, u_star, c_square, c_star, gamma1, m, j, l, half_l, decomp, W1, W2, W3, Q1, Q2, Q3);
+    decomposition(H_star, u_star, c_square, c_star, gamma1, m, j, l, hl, decomp, W1, W2, W3, Q1, Q2, Q3);
 
     local_WENO_3_inter_Z(h, Q1);
     local_WENO_3_inter_Z(h, Q2);
@@ -98,25 +98,25 @@ void WENO_30
     QL[1] = Q2[l]; QR[1] = Q2[l+1];
     QL[2] = Q3[l]; QR[2] = Q3[l+1];
     recomposition(H_star, u_star, c_star, decomp, QL, QR, PL, PR);
-    rho_L[j-half_l] = PL[0]; rho_R[j-half_l] = PR[0];
-    u_L[j-half_l] = PL[1];   u_R[j-half_l] = PR[1];
-    p_L[j-half_l] = PL[2];   p_R[j-half_l] = PR[2];
+    rho_L[j-hl] = PL[0]; rho_R[j-hl] = PR[0];
+    u_L[j-hl] = PL[1];   u_R[j-hl] = PR[1];
+    p_L[j-hl] = PL[2];   p_R[j-hl] = PR[2];
     QL[0] = DQQ[0][0]; QR[0] = DQQ[0][1];
     QL[1] = DQQ[1][0]; QR[1] = DQQ[1][1];
     QL[2] = DQQ[2][0]; QR[2] = DQQ[2][1];
     recomposition(H_star, u_star, c_star, decomp, QL, QR, PL, PR);
-    D_rho_L[j-half_l] = PL[0]; D_rho_R[j-half_l] = PR[0];
-    D_u_L[j-half_l] = PL[1];   D_u_R[j-half_l] = PR[1];
-    D_p_L[j-half_l] = PL[2];   D_p_R[j-half_l] = PR[2];
+    D_rho_L[j-hl] = PL[0]; D_rho_R[j-hl] = PR[0];
+    D_u_L[j-hl] = PL[1];   D_u_R[j-hl] = PR[1];
+    D_p_L[j-hl] = PL[2];   D_p_R[j-hl] = PR[2];
 
-    u_L[j-3] = u_L[j-3] / rho_L[j-3];
-    u_R[j-3] = u_R[j-3] / rho_R[j-3];
-    p_L[j-3] = (p_L[j-3] - 0.5*u_L[j-3]*u_L[j-3]*rho_L[j-3])*(gamma-1.0);
-    p_R[j-3] = (p_R[j-3] - 0.5*u_R[j-3]*u_R[j-3]*rho_R[j-3])*(gamma-1.0);
+    u_L[j-hl] = u_L[j-hl] / rho_L[j-hl];
+    u_R[j-hl] = u_R[j-hl] / rho_R[j-hl];
+    p_L[j-hl] = (p_L[j-hl] - 0.5*u_L[j-hl]*u_L[j-hl]*rho_L[j-hl])*(gamma-1.0);
+    p_R[j-hl] = (p_R[j-hl] - 0.5*u_R[j-hl]*u_R[j-hl]*rho_R[j-hl])*(gamma-1.0);
 
-    D_u_L[j-3] = (D_u_L[j-3] - u_L[j-3]*D_rho_L[j-3]) / rho_L[j-3];
-    D_u_R[j-3] = (D_u_R[j-3] - u_R[j-3]*D_rho_R[j-3]) / rho_R[j-3];
-    D_p_L[j-3] = (D_p_L[j-3] - 0.5*D_rho_L[j-3]*u_L[j-3]*u_L[j-3] - rho_L[j-3]*u_L[j-3]*D_u_L[j-3])*(gamma-1.0);
-    D_p_R[j-3] = (D_p_R[j-3] - 0.5*D_rho_R[j-3]*u_R[j-3]*u_R[j-3] - rho_R[j-3]*u_R[j-3]*D_u_R[j-3])*(gamma-1.0);
+    D_u_L[j-hl] = (D_u_L[j-hl] - u_L[j-hl]*D_rho_L[j-hl]) / rho_L[j-hl];
+    D_u_R[j-hl] = (D_u_R[j-hl] - u_R[j-hl]*D_rho_R[j-hl]) / rho_R[j-hl];
+    D_p_L[j-hl] = (D_p_L[j-hl] - 0.5*D_rho_L[j-hl]*u_L[j-hl]*u_L[j-hl] - rho_L[j-hl]*u_L[j-hl]*D_u_L[j-hl])*(gamma-1.0);
+    D_p_R[j-hl] = (D_p_R[j-hl] - 0.5*D_rho_R[j-hl]*u_R[j-hl]*u_R[j-hl] - rho_R[j-hl]*u_R[j-hl]*D_u_R[j-hl])*(gamma-1.0);
   }
 }
